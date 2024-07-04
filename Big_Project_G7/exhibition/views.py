f568f9531359beac42cead5bed91a70b896cc5c3
hsRyu <ronnie1999@naver.com>
2024-07-04 23:36:44 +0900

    refactoring exhibition/views.py

--- Big_Project_G7/exhibition/views.py
+++ Big_Project_G7/exhibition/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.core.serializers.json import DjangoJSONEncoder
-from django.http import JsonResponse, HttpResponse
-
+from django.http import JsonResponse
 
 from accounts.models import Profile
 
@@ -14,15 +13,14 @@ from .forms import BoothForm
 
 import json
 import requests
-import base64
 
 PORT = 5000
-# AI_API_URL = 'https://8rgyr184rzf1v9-' + str(PORT) + '.proxy.runpod.net/generate/once'
 AI_API_URL = 'https://8rgyr184rzf1v9-' + str(PORT) + '.proxy.runpod.net/generate'
 
 # def getExhidb
 booth_info = Booth_Info.objects.all()
 
+# 전시회 개최시 권한 바꿈(active>staff)
 @login_required
 def change_perm(request):
     if request.method == 'POST':
@@ -35,26 +33,32 @@ def change_perm(request):
 # 전시회 객체 생성
 @login_required
 def create_exhibition(request):
-    if request.method == 'POST':
-        form = ExhibitionForm(request.POST)
-        if form.is_valid():
-            exhibition = form.save(commit=False)
-            exhibition.host_id = request.user.profile.name  # 로그인한 사용자의 아이디를 설정
-            exhibition.save()
-            request.session['data'] = create_json(form)
-        elif not request.session['data']:
-            print(form.errors) # 폼에러 확인
-
-        response, images = get_image_from_server(request.session['data'])
+    # render generated image
+    def render_image(session_data):
+        response, images = get_image_from_server(session_data)
         if response.status_code == 200:
             return render(request, 'layout2.html', {'image_url': images})
         else:
             return response
+        
+    if request.method == 'POST':
+        if request.session['data']:
+           return render_image(request.session['data'])
+        else:
+            form = ExhibitionForm(request.POST)
+            if form.is_valid():
+                exhibition = form.save(commit=False)
+                exhibition.host_id = request.user.profile.name  # 로그인한 사용자의 아이디를 설정
+                exhibition.save()
+                request.session['data'] = create_json(form)
+                return render_image(request.session['data'])
+            else:
+                print(form.errors) # 폼에러 확인
     else:
         form = ExhibitionForm()
     return render(request, 'layout2.html', {'form': form})
 
-# 전시회 개최시 권한 바꿈(active>staff)
+# get generated Image from AI Server
 def get_image_from_server(data:json):
     try:
         response = requests.post(
@@ -63,16 +67,16 @@ def get_image_from_server(data:json):
             headers={'Content-type':'application/json'},
             timeout=200
         )
-
         if response.status_code == 200:
             images = response.json()
             return response, images
         else:
-            return JsonResponse({'Image generation failed'}, status=500), None
+            return JsonResponse({'Image generation failed from server'}, status=500), None
     
     except requests.exceptions.RequestException as E:
         return JsonResponse({'error': str(E)}, status=500), None
 
+# create json file to generate booth layout
 def create_json(form):
     form_data = {
         'exhibition_name': form.cleaned_data['exhibition_name'] ,
