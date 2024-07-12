a96baab9d2ed9e991422e016372ec80f103b4af3
wlehd12 <125344095+wlehd12@users.noreply.github.com>
2024-07-12 17:01:44 +0900

    #3

--- Big_Project_G7/static/js/uiSidebar.js
+++ Big_Project_G7/static/js/uiSidebar.js
@@ -29,6 +29,40 @@ document.addEventListener("DOMContentLoaded", function() {
         console.error('One or more elements not found.');
     }
 
+    // Modal close functionality
+    const closeButtons = document.getElementsByClassName('close');
+    const bookMModal = document.getElementById('bookMModal');
+
+    // Sidebar bookmark list button event
+    const sidebarShowBookmarksButton = document.getElementById('bookMlist');
+    if (sidebarShowBookmarksButton) {
+        sidebarShowBookmarksButton.addEventListener('click',  function() {
+            let bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
+            if (bookmarks.length > 0) {
+                let bookmarkNames = booths.filter(booth => bookmarks.includes(booth.pk)).map(booth => booth.fields.booth_name);
+                let modalBody = document.getElementById('bookmarkli');
+                modalBody.innerHTML = ''; // Clear previous content
+    
+                bookmarkNames.forEach(function(bookmark) {
+                    let li = document.createElement('li');
+                    li.textContent = bookmark;
+                    modalBody.appendChild(li);
+                });
+    
+                bookMModal.style.display = 'block'; // Show modal
+            } else {
+                alert('북마크된 부스 없음.');
+            }
+        });
+    }
+
+    Array.from(closeButtons).forEach(function(btn) {
+        btn.addEventListener('click', function() {
+            bookMModal.style.display = 'none';
+        });
+    });
+
+
     // // Example code for handling bookmarks
     // const listButton = document.getElementById('personal-bookmarks');
     // if (listButton) {
