9964fd190f93ae6b90838ce870db5e7e637e62f5
hsRyu <ronnie1999@naver.com>
2024-07-15 10:36:50 +0900

    chatbot_response_loading_added

--- Big_Project_G7/media/booth_program/admin.py
+++
@@ -1,7 +0,0 @@
-# booth_program/admin.py
-from django.contrib import admin
-from .models import Program, BoothProgramReservation, ReservationTime
-
-admin.site.register(Program)
-admin.site.register(BoothProgramReservation)
-admin.site.register(ReservationTime)
