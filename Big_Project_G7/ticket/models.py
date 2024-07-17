1a2cdc10a88b077e05deec0c384c28ad1b5620f8
hsRyu <ronnie1999@naver.com>
2024-07-18 02:47:29 +0900

    티켓 기능 구현 추가 완료. (전시회 하나에 예약일 여러개 및 버그 수정)

--- Big_Project_G7/ticket/models.py
+++ Big_Project_G7/ticket/models.py
@@ -4,7 +4,7 @@ class TicketBoughtInfo(models.Model):
     userticketid = models.AutoField(db_column='UserTicketID', primary_key=True)  # Field name made lowercase.
     user_id = models.IntegerField()
     exhibitionid = models.IntegerField(db_column='ExhibitionID')  # Field name made lowercase.
-    ticketid = models.IntegerField(db_column='TicketID', unique=True)  # Field name made lowercase.
+    ticketid = models.CharField(db_column='TicketID', max_length=100, unique=True)  # Field name made lowercase.
     adult = models.IntegerField(db_column='adult')
     child = models.IntegerField(db_column='child')
     reservationDate = models.DateField(db_column='reservationDate')
--- Big_Project_G7/ticket/views.py
+++ Big_Project_G7/ticket/views.py
@@ -18,7 +18,6 @@ def get_ticket(userid) -> list:
     tickets = []
     for ticket in ticket_list:
         obj = ExhibitionInfo.objects.get(pk=ticket.exhibitionid)
-        print(obj)
         name = obj.ExhibitionName
         img = obj.ExhibitionImageURL
         adult = ticket.adult
@@ -39,44 +38,44 @@ def ticket_list(request):
 # 티켓 세부사항
 @login_required
 def ticket_detail(request, ticket_id):
-    if request.method == 'POST':
-        ticket = TicketBoughtInfo.objects.get(ticketid=ticket_id)
-        exhibition = ExhibitionInfo.objects.get(pk=ticket.exhibitionid)
-        exhibition_name = exhibition.exhibition_name
-        hall = ExhibitionHall.objects.get(ExhibitionHallID=exhibition.hall_id).ExhibitionHallDescription
-        adult = ticket.adult
-        child = ticket.child
-        username = request.user.username
-        date = str(ticket.reservationDate)
+    ticket = TicketBoughtInfo.objects.get(ticketid=ticket_id)
+    exhibition_id = ticket.exhibitionid
+    exhibition = ExhibitionInfo.objects.get(ExhibitionID=exhibition_id)
+    exhibition_name = exhibition.ExhibitionName
+    hall = ExhibitionHall.objects.get(pk=exhibition_id).ExhibitionHallDescription
+    adult = ticket.adult
+    child = ticket.child
+    username = request.user.username
+    date = str(ticket.reservationDate)
 
-        context = {'ticket_id':int(ticket_id), 'hall':hall, 'exhibition_name':exhibition_name,
-                        'username':username, 'adult':adult, 'child':child, 'date':date}
-        
-        ticket_data = json.dumps(context, ensure_ascii=False)
-        img = generate_QR(ticket_data)
-        
-        context['image'] = img
-        return render(request, 'ticket_detail.html', context)
-    else:
-        return redirect('ticket:ticket_list')
+    context = {'ticket_id':int(ticket_id), 'hall':hall, 'exhibition_name':exhibition_name,
+                    'username':username, 'adult':adult, 'child':child, 'date':date}
 
+    ticket_data = json.dumps(context, ensure_ascii=False)
+    img = generate_QR(ticket_data)
+    
+    context['image'] = img
+    return render(request, 'ticket_detail.html', context)
+
+# 티켓 구매 -> refactoring 필수
 @login_required
 def purchase_ticket(request):
     if request.method == 'POST':
         form = TicketReservationForm(request.POST)
         userid = request.user.id
+        reservationdate = request.POST.get('reservationDate')
+        dateKey = ''.join(reservationdate.split('-'))
         if form.is_valid():
             reservation = form.save(commit=False)
             reservation.user_id = userid
-            ticketid = int(''.join([str(userid),str(reservation.exhibitionid)]))
+            ticketid = int(''.join([str(userid),str(reservation.exhibitionid), dateKey]))
             reservation.ticketid = ticketid
         else:
-            reservationdate = request.POST.get('reservationDate')
             exhibitionid = request.POST.get('exhibition_name')
             if exhibitionid is not None:
-                ticketid = int(''.join([str(userid),str(exhibitionid)]))
+                ticketid = int(''.join([str(userid),str(exhibitionid), dateKey]))
                 reservation = TicketBoughtInfo(
-                    exhibitionid=exhibitionid,
+                    exhibitionid=int(exhibitionid),
                     user_id=userid,
                     ticketid=ticketid,
                     adult=request.POST.get('adult'),
@@ -97,6 +96,7 @@ def purchase_ticket(request):
 
     return render(request, 'purchase_ticket.html', {'form': form})
 
+# 티켓 구매 취소
 @login_required
 def cancel_ticket(request, ticket_id):
     if request.method == 'POST':
@@ -104,21 +104,21 @@ def cancel_ticket(request, ticket_id):
         return redirect('ticket:ticket_list')
     else:
         return redirect('ticket:ticket_list')
-    
+
+# 부스 선택 시 예약 가능한 날짜 찾아주는 API
 def check_availableDate(request, exhibition_id):
     try:
         exhibition = ExhibitionInfo.objects.get(pk=exhibition_id)
-        print(exhibition)
         start_date = exhibition.ExhibitionRegistrationDate
         end_date = exhibition.ExhibitionClosedDate
-        print(start_date, end_date)
         date_available = get_date_choice(start_date, end_date)
 
         return JsonResponse({'dates':date_available})
     
     except:
         return JsonResponse({'dates': []})
-    
+
+# 예약 가능한 날짜 찾기
 def get_date_choice(start_date, end_date):
     return [(start_date + timedelta(days=i)).strftime('%Y-%m-%d')
                 for i in range((end_date - start_date).days + 1)]
